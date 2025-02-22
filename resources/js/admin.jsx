89bace3ad69d9c806ffe6b448a4be780be6f3176
Laravel <abolfazl19910r@gmail.com>
2025-02-22 18:44:33 +0330

    Update admin.jsx

--- resources/js/admin.jsx
+++ resources/js/admin.jsx
@@ -2,10 +2,15 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import AdminDashboard from './Components/Admin/AdminDashboard';
 import { LoyaltyAdmin, AnnouncementAdmin } from './components/admin/LoyaltyAdmin';
 import { BlogAdmin, GalleryAdmin } from './components/admin/BlogAdmin';
 
 // Mount admin components
+if (document.getElementById('admin-dashboard')) {
+    createRoot(document.getElementById('admin-dashboard')).render(<AdminDashboard />);
+}
+
 if (document.getElementById('admin-loyalty')) {
     createRoot(document.getElementById('admin-loyalty')).render(<LoyaltyAdmin />);
 }
