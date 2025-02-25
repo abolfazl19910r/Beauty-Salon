a683b8b419ec9cd8d91d93127bea8753c4a0876f
Laravel <abolfazl19910r@gmail.com>
2025-02-25 21:34:20 +0330

    Create resources\views\auth\two-factor-auth.blade.php

---
+++ resources/views/auth/two-factor-auth.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'تایید دو مرحله‌ای')
+
+@section('content')
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="max-w-md w-full">
+            <div id="two-factor-auth"></div>
+        </div>
+    </div>
+@endsection
