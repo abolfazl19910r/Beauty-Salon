3b789046f706897a70baa16cf30b35832b85297d
Laravel <abolfazl19910r@gmail.com>
2025-02-25 21:25:50 +0330

    Delete Files

--- resources/views/blog/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container mx-auto px-4 py-8">
-        <h1 class="text-2xl font-bold mb-6">بلاگ و مقالات آموزشی</h1>
-        <div id="blog-root"></div>
-    </div>
-@endsection
