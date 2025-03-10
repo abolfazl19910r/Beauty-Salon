325c473853bae132cce9f4e5d0bd4325122eb8ed
Laravel <abolfazl19910r@gmail.com>
2025-03-10 23:20:10 +0330

    Add Routes

--- routes/admin/bookings.php
+++ routes/admin/bookings.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('bookings')->name('bookings.')->group(function () {
     Route::get('/', [AdminBookingController::class, 'index'])->name('index');
-
+    Route::get('/create', [AdminBookingController::class, 'create'])->name('create');
+    Route::post('/', [AdminBookingController::class, 'store'])->name('store');
+    Route::get('/{booking}/edit', [AdminBookingController::class, 'edit'])->name('edit');
     Route::get('/{booking}', [AdminBookingController::class, 'show'])->name('show');
     Route::put('/{booking}', [AdminBookingController::class, 'update'])->name('update');
     Route::delete('/{booking}', [AdminBookingController::class, 'destroy'])->name('destroy');
