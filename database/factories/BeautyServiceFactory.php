4f9930bdd44c7747ec1f666fffda8b8c0e3f7196
Laravel <abolfazl19910r@gmail.com>
2025-03-10 08:11:55 +0330

    Refactored BeautyServiceFactory

--- database/factories/BeautyServiceFactory.php
+++ database/factories/BeautyServiceFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\ServiceCategory;
+use App\Services\CategoryService;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BeautyServiceFactory extends Factory
@@ -15,7 +15,7 @@ class BeautyServiceFactory extends Factory
             'description' => fake()->paragraph(),
             'price' => fake()->numberBetween(50000, 1000000),
             'duration' => fake()->randomElement([30, 45, 60, 90, 120]),
-            'category_id' => ServiceCategory::factory()
+            'category_id' => CategoryService::factory()
         ];
     }
 }
