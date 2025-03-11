ce562a723932e54e66fb1709f105074eb98bba9b
Laravel <abolfazl19910r@gmail.com>
2025-03-11 11:07:10 +0330

    Update Models

--- app/Models/Holiday.php
+++ app/Models/Holiday.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 
 class Holiday extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'specialist_id',
         'date',
--- app/Models/WorkSchedule.php
+++ app/Models/WorkSchedule.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 
 class WorkSchedule extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'specialist_id',
         'work_days',
