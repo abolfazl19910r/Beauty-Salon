0dae6a1a9f6d7b90c718af397203365814a7ada1
Laravel <abolfazl19910r@gmail.com>
2025-03-15 19:32:30 +0330

    Update EventServiceProvider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Booking;
+use App\Models\DiscountCode;
 use App\Observers\BookingObserver;
+use App\Observers\DiscountCodeObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -19,5 +21,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Booking::observe(BookingObserver::class);
+        DiscountCode::observe(DiscountCodeObserver::class);
     }
 }
