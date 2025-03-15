55927d4f5702d67adcf9aca3ccfc016b9128ce02
Laravel <abolfazl19910r@gmail.com>
2025-03-15 18:56:14 +0330

    Create config\middleware-aliases.php

---
+++ config/middleware-aliases.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    // Laravel Core Middleware Aliases
+    'auth' => \App\Http\Middleware\Authenticate::class,
+    'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+    'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+    'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+    'can' => \Illuminate\Auth\Middleware\Authorize::class,
+    'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+    'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+    'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+    'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+    'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+    'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+    // Custom Middleware Aliases
+    'admin' => \App\Http\Middleware\AdminMiddleware::class,
+    'check.booking.ownership' => \App\Http\Middleware\CheckBookingOwnership::class,
+];
