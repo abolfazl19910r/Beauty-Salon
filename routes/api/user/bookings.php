d69190983c37fdf82cec907a57bfd0851f5e0fd2
Laravel <abolfazl19910r@gmail.com>
2025-03-15 19:19:54 +0330

    Update Route bookings

--- routes/api/user/bookings.php
+++ routes/api/user/bookings.php
@@ -3,16 +3,24 @@
 use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('bookings')->group(function () {
-    Route::get('/user', [BookingController::class, 'getUserBookings']);
+Route::prefix('bookings')->middleware('auth:sanctum')->group(function () {
+
+    Route::get('/', [BookingController::class, 'getUserBookings']);
     Route::get('/upcoming', [BookingController::class, 'getUpcomingBookings']);
     Route::get('/past', [BookingController::class, 'getPastBookings']);
+    Route::get('/latest', [BookingController::class, 'latestSuccessful']);
+
+    Route::post('/', [BookingController::class, 'store']);
 
     Route::get('/{booking}', [BookingController::class, 'show']);
-    Route::post('/', [BookingController::class, 'store']);
     Route::post('/{booking}/reschedule', [BookingController::class, 'reschedule']);
     Route::post('/{booking}/cancel', [BookingController::class, 'cancel']);
-
     Route::post('/{booking}/rate', [BookingController::class, 'rate']);
     Route::post('/{booking}/apply-discount', [BookingController::class, 'applyDiscount']);
+
+    Route::get('/services', [BookingController::class, 'getServices']);
+    Route::get('/specialists/{service_id}', [BookingController::class, 'getSpecialistsByService']);
+    Route::get('/available-dates/{specialist}', [BookingController::class, 'getAvailableDates']);
+    Route::get('/time-slots/{specialist}/{date}', [BookingController::class, 'getAvailableTimeSlots']);
+    Route::post('/check-discount', [BookingController::class, 'checkDiscount']);
 });
