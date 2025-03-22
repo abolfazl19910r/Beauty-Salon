9bd58ae0488feb0bec68845a400a9ffbbb9dd815
Laravel <abolfazl19910r@gmail.com>
2025-03-22 22:56:53 +0330

    Add Route Loyalty

--- routes/admin/loyalty.php
+++ routes/admin/loyalty.php
@@ -11,4 +11,10 @@ Route::prefix('loyalty')->name('loyalty.')->group(function () {
     Route::get('/{loyalty}/edit', [AdminLoyaltyController::class, 'edit'])->name('edit');
     Route::put('/{loyalty}', [AdminLoyaltyController::class, 'update'])->name('update');
     Route::delete('/{loyalty}', [AdminLoyaltyController::class, 'destroy'])->name('destroy');
+
+    Route::post('/rewards/{reward}/redeem', [AdminLoyaltyController::class, 'redeemReward'])->name('redeem-reward');
+    Route::get('/points', [AdminLoyaltyController::class, 'getPoints'])->name('points');
+    Route::get('/rewards', [AdminLoyaltyController::class, 'getRewards'])->name('rewards');
+    Route::get('/history', [AdminLoyaltyController::class, 'getHistory'])->name('history');
+    Route::get('/export', [AdminLoyaltyController::class, 'export'])->name('export');
 });
