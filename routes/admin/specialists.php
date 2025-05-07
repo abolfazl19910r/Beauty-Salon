1de3a54a7dd314aaba47d68ea8e82af25cd2e0d3
Laravel <abolfazl19910r@gmail.com>
2025-05-07 19:27:48 +0330

    Update Route specialists

--- routes/admin/specialists.php
+++ routes/admin/specialists.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminSpecialistController;
+use App\Http\Controllers\Admin\AdminSpecialistScheduleController;
+use App\Http\Controllers\Admin\AdminSpecialistLeaveController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('specialists')->name('specialists.')->group(function () {
@@ -12,4 +14,16 @@ Route::prefix('specialists')->name('specialists.')->group(function () {
     Route::get('/{specialist}/edit', [AdminSpecialistController::class, 'edit'])->name('edit');
     Route::put('/{specialist}', [AdminSpecialistController::class, 'update'])->name('update');
     Route::delete('/{specialist}', [AdminSpecialistController::class, 'destroy'])->name('destroy');
+
+    Route::get('/{specialist}/schedules/edit', [AdminSpecialistScheduleController::class, 'edit'])
+        ->name('schedules.edit');
+    Route::put('/{specialist}/schedules', [AdminSpecialistScheduleController::class, 'update'])
+        ->name('schedules.update');
+
+    Route::get('/{specialist}/leaves', [AdminSpecialistLeaveController::class, 'index'])
+        ->name('leaves.index');
+    Route::post('/{specialist}/leaves', [AdminSpecialistLeaveController::class, 'store'])
+        ->name('leaves.store');
+    Route::put('/{specialist}/leaves/{leave}', [AdminSpecialistLeaveController::class, 'update'])
+        ->name('leaves.update');
 });
