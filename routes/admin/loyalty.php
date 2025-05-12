b90e6f17460f7aee0924fcd7277644414f7b4083
Laravel <abolfazl19910r@gmail.com>
2025-05-12 05:56:52 +0330

    Refactored Route

--- routes/admin/loyalty.php
+++ routes/admin/loyalty.php
@@ -3,18 +3,18 @@
 use App\Http\Controllers\Admin\AdminLoyaltyController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AdminLoyaltyController::class)->group(function () {
-    Route::get('/loyalty', 'index')->name('loyalty.index');
-    Route::get('/loyalty/create', 'create')->name('loyalty.create');
-    Route::post('/loyalty', 'store')->name('loyalty.store');
-    Route::get('/loyalty/{reward}', 'show')->name('loyalty.show');
-    Route::get('/loyalty/{reward}/edit', 'edit')->name('loyalty.edit');
-    Route::put('/loyalty/{reward}', 'update')->name('loyalty.update');
-    Route::delete('/loyalty/{reward}', 'destroy')->name('loyalty.destroy');
+Route::prefix('loyalty')->name('loyalty.')->group(function () {
+    Route::get('/points', [AdminLoyaltyController::class, 'getPoints'])->name('points');
+    Route::get('/rewards', [AdminLoyaltyController::class, 'getRewards'])->name('rewards');
+    Route::get('/history', [AdminLoyaltyController::class, 'getHistory'])->name('history');
+    Route::get('/create', [AdminLoyaltyController::class, 'create'])->name('create');
+    Route::get('/export', [AdminLoyaltyController::class, 'export'])->name('export');
+    Route::post('/rewards/{reward}/redeem', [AdminLoyaltyController::class, 'redeemReward'])->name('redeem-reward');
 
-    Route::get('/loyalty/points', 'getPoints')->name('loyalty.points');
-    Route::get('/loyalty/rewards', 'getRewards')->name('loyalty.rewards');
-    Route::get('/loyalty/history', 'getHistory')->name('loyalty.history');
-    Route::post('/loyalty/rewards/{reward}/redeem', 'redeemReward')->name('loyalty.redeem-reward');
-    Route::get('/loyalty/export', 'export')->name('loyalty.export');
+    Route::get('/', [AdminLoyaltyController::class, 'index'])->name('index');
+    Route::post('/', [AdminLoyaltyController::class, 'store'])->name('store');
+    Route::get('/{reward}', [AdminLoyaltyController::class, 'show'])->name('show');
+    Route::get('/{reward}/edit', [AdminLoyaltyController::class, 'edit'])->name('edit');
+    Route::put('/{reward}', [AdminLoyaltyController::class, 'update'])->name('update');
+    Route::delete('/{reward}', [AdminLoyaltyController::class, 'destroy'])->name('destroy');
 });
