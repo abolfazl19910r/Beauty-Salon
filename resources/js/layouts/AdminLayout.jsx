3cca825d8568374deeefc4825e2d82b624c28f44
Laravel <abolfazl19910r@gmail.com>
2025-05-30 13:21:14 +0330

    Update AdminLayout.jsx

--- resources/js/layouts/AdminLayout.jsx
+++ resources/js/layouts/AdminLayout.jsx
@@ -1,19 +1,21 @@
-//resources/js/layouts/AdminLayout.jsx
 import React from 'react';
 import Sidebar from '@/components/admin/Sidebar';
 import Header from '@/components/admin/Header';
 
 const AdminLayout = ({ children }) => {
     return (
-        <div className="flex h-screen bg-gray-100">
-            <Sidebar />
-            <div className="flex-1 flex flex-col overflow-hidden">
-                <Header />
-                <main className="flex-1 overflow-y-auto p-4">
-                    {children}
-                </main>
+        <>
+            <meta name="csrf-token" content={document.querySelector('meta[name="csrf-token"]')?.content || ''} />
+            <div className="flex h-screen bg-gray-100">
+                <Sidebar />
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    <Header />
+                    <main className="flex-1 overflow-y-auto p-4">
+                        {children}
+                    </main>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
