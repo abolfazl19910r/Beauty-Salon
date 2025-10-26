3c3c84774350c4a819fd4ec66c500ebe4738303f
Abolfazl <abolfazl19910r@gmail.com>
2025-10-26 18:41:59 +0330

    refactor(routes): improve booking management routes order
    - Move /stats route to the beginning of the list
    - Prevent conflicts with parameterized {booking} routes
    - Ensure correct route priority handling in Laravel

--- routes/admin/bookings.php
+++ routes/admin/bookings.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminBookingController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('bookings')->name('bookings.')->group(function () {
+    Route::get('/stats', [AdminBookingController::class, 'getStats'])->name('stats');
     Route::get('/', [AdminBookingController::class, 'index'])->name('index');
     Route::get('/create', [AdminBookingController::class, 'create'])->name('create');
     Route::post('/', [AdminBookingController::class, 'store'])->name('store');
@@ -11,5 +12,4 @@ Route::prefix('bookings')->name('bookings.')->group(function () {
     Route::get('/{booking}', [AdminBookingController::class, 'show'])->name('show');
     Route::put('/{booking}', [AdminBookingController::class, 'update'])->name('update');
     Route::delete('/{booking}', [AdminBookingController::class, 'destroy'])->name('destroy');
-    Route::get('/stats', [AdminBookingController::class, 'getStats'])->name('stats');
 });
