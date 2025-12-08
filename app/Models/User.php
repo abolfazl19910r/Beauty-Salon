ccd904507fb54201bde2380730630306e788c45d
Abolfazl <abolfazl19910r@gmail.com>
2025-12-08 08:07:35 +0330

    feat(models): add specialist relationship to User model
    - Add specialist() hasOne relationship
    - Link User to Specialist via phone number

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -186,5 +187,10 @@ class User extends Authenticatable
     public function getAllPermissions()
     {
         return $this->roles->flatMap->permissions->unique('id');
+    }
+
+    public function specialist(): HasOne
+    {
+        return $this->hasOne(Specialist::class, 'phone', 'phone');
     }
 }
