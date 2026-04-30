66d4160822d9347978351b83d86f8dfb6c018b5c
Abolfazl <abolfazl19910r@gmail.com>
2026-04-30 18:20:31 +0330

    Fix/bug-fixx

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
-class BeautyService extends Model
+class Booking extends Model
 {
     protected $table = 'beauty_services';
 
